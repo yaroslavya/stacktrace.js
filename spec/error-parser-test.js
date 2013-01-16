2d369d7a2f0939d89f1011e3f927804d42050892
Eric Wendelin <emwendelin@gmail.com>
2013-01-16 15:47:57 -0700

    Partial V8 impl

--- spec/error-parser-test.js
+++ spec/error-parser-test.js
@@ -6,15 +6,15 @@ describe('error-parser.js', function() {
                 expect(unit.chooseParser(CapturedExceptions.chrome_15)).toBe(unit.parseV8);
             });
             it('should detect Firefox', function() {
-                expect(unit.chooseParser(CapturedExceptions.firefox_36)).toBe(unit.parseFIREFOX);
-                expect(unit.chooseParser(CapturedExceptions.firefox_7)).toBe(unit.parseFIREFOX);
-                expect(unit.chooseParser(CapturedExceptions.firefox_14)).toBe(unit.parseFIREFOX);
+                expect(unit.chooseParser(CapturedExceptions.firefox_36)).toBe(unit.parseSpiderMonkey);
+                expect(unit.chooseParser(CapturedExceptions.firefox_7)).toBe(unit.parseSpiderMonkey);
+                expect(unit.chooseParser(CapturedExceptions.firefox_14)).toBe(unit.parseSpiderMonkey);
             });
             it('should detect Safari', function() {
-                expect(unit.chooseParser(CapturedExceptions.safari_6)).toBe(unit.parseSAFARI);
+                expect(unit.chooseParser(CapturedExceptions.safari_6)).toBe(unit.parseNitro);
             });
             it('should detect IE', function() {
-                expect(unit.chooseParser(CapturedExceptions.ie_10)).toBe(unit.parseIE);
+                expect(unit.chooseParser(CapturedExceptions.ie_10)).toBe(unit.parseChakra);
             });
             it('should detect Opera', function() {
                 expect(unit.chooseParser(CapturedExceptions.opera_854)).toBe(unit.parseOPERA);
@@ -27,10 +27,10 @@ describe('error-parser.js', function() {
             });
         });
 
-        describe('#parseSAFARI', function() {
+        describe('#parseNitro', function() {
             var unit = new ErrorParser();
             it('should parse Safari 6 Errors', function() {
-                var errorInfo = unit.parseSAFARI(CapturedExceptions.safari_6);
+                var errorInfo = unit.parseNitro(CapturedExceptions.safari_6);
                 expect(errorInfo.stack).toBeTruthy();
                 expect(errorInfo.stack.length).toBe(3);
                 expect(errorInfo.stack[0]).toEqual({fn: '{anonymous}', args: [], src: 'scheme://path/to/file.js', line: '48', char: undefined});
@@ -38,6 +38,15 @@ describe('error-parser.js', function() {
                 expect(errorInfo.stack[2]).toEqual({fn: 'onclick', args: [], src: 'scheme://path/to/file.js', line: '82', char: undefined});
                 expect(errorInfo.message).toBeTruthy();
                 expect(errorInfo.message).toEqual("'null' is not an object (evaluating 'x.undef')");
+            });
+        });
+
+        describe('#parseV8', function() {
+            var unit = new ErrorParser();
+            it('should parse V8 Errors', function() {
+                var errorInfo = unit.parseV8(CapturedExceptions.chrome_15);
+                expect(errorInfo.stack).toBeTruthy();
+                expect(errorInfo.stack.length).toBe(3);
             });
         });
     });
